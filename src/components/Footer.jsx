cac964614de304edcbdfd0a8d1501c62d3b2c96a
Alfredappiagyei <appiagyeialfred815@gmail.com>
2024-02-12 12:25:44 +0000

    fix href erro in anchor

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -51,7 +51,7 @@ export default function Footer() {
         </div>
         <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <div class="sm:flex sm:items-center sm:justify-between">
-          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="#" class="hover:underline">Soccermissionaries™</a>. All Rights Reserved.
+          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://safa.com/" class="hover:underline">Soccermissionaries™</a>. All Rights Reserved.
           </span>
           <div class="flex mt-4 sm:justify-center sm:mt-0">
             <div class="relative">
